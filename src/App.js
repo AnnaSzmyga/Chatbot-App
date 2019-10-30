328d57c601d2edfbb360284af765ee0ca102b62f
Anna Szmyga <anna.szmyga@o2.pl>
2019-10-30 10:07:44 +0100

    Set delay for bot responses

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MessagesList from './components/MessagesList/MessagesList';
 import MessageBox from './components/MessageBox/MessageBox';
 import './App.scss';
 
-const url = 'https://acobot-brainshop-ai-v1.p.rapidapi.com/get?bid=178&key=sX5A2PcYZbsN5EY6&uid=mashape&msg='
+ const url = 'https://acobot-brainshop-ai-v1.p.rapidapi.com/get?bid=178&key=sX5A2PcYZbsN5EY6&uid=mashape&msg=';
 
 
 class App extends React.Component {
@@ -18,7 +18,7 @@ class App extends React.Component {
   handleChange = (e) => {
     this.setState({
       inputText: e.target.value,
-      msgText: e.target.value.trim().replace(/ /g, '%')
+      msgText: encodeURI(e.target.value.trim())
     });
   }
 
@@ -26,21 +26,25 @@ class App extends React.Component {
     e.preventDefault();
     if (this.state.msgText !== '') {
       this.addMessage(this.state.inputText, true);
-      this.runBot(this.state.msgText);
+      this.getBotAnswer(this.state.msgText);
     }
     this.setState({ inputText: '' });
   }
 
   addMessage = (content, userMsg) => {
-    this.setState({
+    const delay = userMsg ? 0 : 1000;
+    setTimeout(() => this.setState({
       messages: [...this.state.messages, { content, userMsg }]
-    });
+      }), delay);
+    // this.setState({
+    //   messages: [...this.state.messages, { content, userMsg }]
+    // });
     // keep scroll down when add message
     const messagesList = document.querySelector('.messages-list');
     messagesList.scrollTop = messagesList.scrollHeight;
   }
 
-  runBot = (msgText) => {
+  getBotAnswer = (msgText) => {
     fetch(url + msgText, {
       "method": "GET",
       "headers": {
